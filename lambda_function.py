5bfa24c323908fc93a3df1fd483118ee66c4c70a
Savita Savita <savita.singh3@vodafone.com>
2024-04-23 16:01:47 +0100

    updated latest code

--- lambda_function.py
+++ lambda_function.py
@@ -1,24 +1,59 @@
 import json
+import boto3
+import base64
+import string
+import random
+import uuid
+import logging
+logger = logging.getLogger()
+logger.setLevel("INFO")
+from boto3.dynamodb.types import TypeSerializer, TypeDeserializer
 
-with open('products.txt', 'r') as f:
-    data = json.load(f)
-print(data)
+ORDER_TABLE = 'OrderDB'
+response = ''
+ddbclient = boto3.client('dynamodb')
+dynamodb = boto3.resource('dynamodb', 'eu-north-1')
+paginator = ddbclient.get_paginator('scan')
 
 def lambda_handler(event, context):
-    print("I am being called")
-    # TODO implement
+    logger.info("GetListOfOrder")
+    logger.info(event);
+    email = event['queryStringParameters']['email']
+
+    logger.info ("GetListOfOrder" + email)
+
+
+    #for page in iterator:
+    #    for item in page['Items']:
+    #        print (json.dumps(item))
+    try:    
+        #response = table.get_item(
+        #    Key={
+        #        'email' : 'savitasingh18@gmail.com'
+        #        }
+        #    )
+            
+        response = ddbclient.query(
+            TableName=ORDER_TABLE,
+            KeyConditionExpression='email = :email',
+            ExpressionAttributeValues={
+                ':email': {'S': email}
+            }
+        )
+        logger.info (json.dumps(response))
+        
+
+    #logger.info(response)
+    
+    except Exception as err:
+        logger.error(err)
+
+    
     return {
-        "isBase64Encoded": "false",
-        'statusCode': 200,
+         'statusCode' : 200,
         "headers": {
-        'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': '*',
-        'Cache-Control': 'public, max-age=14300',
-        'Content-Type': 'application/json'
-
-
-        },
- #       'body': json.dumps('Again This is the next change I am going GetProductList Lambda....!')
-        'body' : json.dumps(data)
+        "Access-Control-Allow-Origin": "*"
+    },
+         'body' : json.dumps(response)
+        
     }
